7d7b97113f23ef357a0b201979c8e5bd4d6117b8
PicolasCage243 <142632758+PicolasCage243@users.noreply.github.com>
2023-09-24 14:32:27 -0300

    conta

---
+++ Programação Orientada ao Objeto em Java/Conta Bancária/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        String cliente = "Pedro";
+        String tipoConta = "Corrente";
+        double saldo = 600; 
+        System.out.println("*******************");
+          System.out.println("\nNome do clente: " + cliente); 
+          System.out.println("Tipo da conta: " + tipoConta);
+          System.out.println("Saldo atual: R$" + saldo);
+          System.out.println("\n*******************");
+
+    
+    }
+}
