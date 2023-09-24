eb45825a4d054640a1c63e71771d780d83c3889b
PicolasCage243 <142632758+PicolasCage243@users.noreply.github.com>
2023-09-24 15:15:03 -0300

    correções

--- Programação Orientada ao Objeto em Java/Conta Bancária/src/App.java
+++ Programação Orientada ao Objeto em Java/Conta Bancária/src/App.java
@@ -18,34 +18,46 @@ public class App {
                 Digite sua opção
                 1 - Consultar saldo
                 2 - Transferir valor
-                3 - Receber valor
+                3 - Depositar valor
                 4 - sair
                 """;
                 Scanner ler = new Scanner(System.in);
-    while(opcao != 4){
+    while(opcao == 0){
         System.out.println(menu);
         opcao = ler.nextInt();
-        if(opcao == 1){
+       try {
+       if(opcao == 1){
             System.out.println("Seu saldo atual é de: R$" + saldo);
+            opcao = 0;
         }
         if(opcao == 2){
+            System.out.println("Digite quanto deseja transferir: ");
             transferir = ler.nextDouble();
             if(saldo < transferir){
                 System.out.println("Saldo insuficiente para efetuar a transferência!");
+                opcao = 0;
             }
             else{
             saldo -= transferir;
             System.out.println("Você fez uma transferência no valor de: R$" + transferir);
+            opcao = 0;
         }
         }
         if(opcao == 3){
+            System.out.println("Digite quanto deseja depositar: ");
             transferir = ler.nextDouble();
             saldo += transferir;
             System.out.println("Você depositou R$" + transferir + " na sua conta.");
+            opcao = 0;
         }
-        else{
-            System.out.println("Opção inválida!");
+        if(opcao == 4){
+            System.out.println("Saindo do programa...");
         }
+
+       } catch (Exception e) {
+        System.out.println("Opção inválida!");
+            opcao = 0;
+       }
     }
     }
 }
