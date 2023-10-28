f3605f4c2a2e5793735c3513e574b4d81e7e6c85
PicolasCage243 <142632758+PicolasCage243@users.noreply.github.com>
2023-10-27 21:58:45 -0300

    encapsulamento

--- Programação Orientada ao Objeto em Java/Aplicando a Orientação a Objeto/ScreenMatch/src/App.java
+++ Programação Orientada ao Objeto em Java/Aplicando a Orientação a Objeto/ScreenMatch/src/App.java
@@ -1,17 +1,19 @@
+import screenmatch.modelos.Filme;
+
 public class App {
     public static void main(String[] args) throws Exception {
        Filme setimo = new Filme();
-     setimo.nome = "O Sétimo Selo";
-     setimo.anoDeLancamento = 1957;
-     setimo.duracaoEmMinutos = 96;
-     setimo.incluidoNoPlano = true;
+     setimo.setNome("O Sétimo Selo");
+     setimo.setAnoDeLancamento(1957);
+     setimo.setAnoDeLancamento(96);
+     setimo.setIncluidoNoPlano(true);
 
      setimo.exibeFicha();
 
      setimo.avalia(7);
         setimo.avalia(9);
         setimo.avalia(6);
-
+        System.out.println("Total de avaliações: "+setimo.getTotalAvaliacoes());
        System.out.println("A média de avaliações é: " + setimo.pegaMedia());
     }
 }
